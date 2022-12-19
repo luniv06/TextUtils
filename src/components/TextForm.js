f04741a15e3ac4dbd8d80459b75f613cc4677569
Vinul James <vinuljames@gmail.com>
2022-12-19 19:53:36 +0530

    adding more logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,6 +5,10 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const handleLowClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     console.log('on change');
     setText(event.target.value);
@@ -12,20 +16,35 @@ export default function TextForm(props) {
 
   const [text, setText] = useState('Enter text here');
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          rows="8"
-          value={text}
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to lowercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your Text Summary: </h2>
+        <p>
+          {' '}
+          {text.split(' ').length} words and {text.length} characters{' '}
+        </p>
+        <p> {0.008 * text.split(' ').length} minutes read </p>
+        <h3> Preview: </h3>
+        <p> {text} </p>
+      </div>
+    </>
   );
 }
