6ee3088c4c77f43a8352f39d5c5d23f016b32cdf
Vinul James <vinuljames@gmail.com>
2022-12-20 13:43:38 +0530

    additional functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,17 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
+  const handleCopy = () => {
+    var text = document.getElementById('myBox');
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(' '));
+  };
+
   const [text, setText] = useState('');
   return (
     <>
@@ -41,6 +52,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-2" onClick={handleLowClick}>
           Convert to lowercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div
